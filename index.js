2998952b0c98d4e61c1a34671d67660318a4e011
paulyeo23 <78999327+paulyeo23@users.noreply.github.com>
2022-02-08 11:45:49 +0800

    Update index.js
    spam

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { writeFile } from 'fs';
+import { writeFile } from "fs";
 let name = process.argv[2];
-writeFile('data.json', name, (err) => console.error('err ->', err));
+writeFile("data.json", name, (err) => console.error("err ->", err));
+// Lobster aux crevette with a moranise sauce topped with truffle patte and spam
